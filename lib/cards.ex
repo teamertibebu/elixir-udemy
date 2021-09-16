30016869fe127608510545df76649872f699017b
Teamer Tibebu <teamer.tibebu@revelry.co>
2021-09-16 13:50:48 -0500

    (create) Cards module, (add) create_deck, shuffle, & contains? functions

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,9 @@
 defmodule Cards do
   def create_deck do 
-    _cards = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
+
+    cards = for val <- values, suit <- suits, do: "#{val} of #{suit}"
   end
 
   def shuffle(deck) do
